99291aacc97cc144b113c3926a05518789631e1b
frazermills <77229450+frazermills@users.noreply.github.com>
2021-02-16 12:58:34 +0000

    Added white cube
    Added the white cube that will soon become the snake. I also created a new function called update_screen() so that pygame.display.flip() is only called once per game tick

--- main.py
+++ main.py
@@ -6,7 +6,12 @@ def setup_screen(RES):
 
 def render_screen(screen):
     screen.fill(pygame.Color("black"))
+
+def update_screen():
     pygame.display.flip()
+
+def draw_snake(screen, snake, SIZE):
+    [pygame.draw.rect(screen, pygame.Color('white'), (i, j, SIZE - 1, SIZE - 1)) for i, j in snake]
 
 def event_handler():
     for event in pygame.event.get():
@@ -24,10 +29,16 @@ def main():
     screen = setup_screen(RES)
     clock = pygame.time.Clock()
 
+    x = random.randrange(SIZE, RES - SIZE, SIZE)
+    y = random.randrange(SIZE, RES - SIZE, SIZE)
+
+    snake = [(x, y)]
+
     while True:
         render_screen(screen)
+        draw_snake(screen, snake, SIZE)
         clock.tick(FPS)
-
+        update_screen()
         event_handler()
 
 if __name__ == "__main__":
