6078439748176b1191e07c7cb7ddbccfbf165da7
frazermills <77229450+frazermills@users.noreply.github.com>
2021-02-21 14:19:33 +0000

    Commented code
    I labeled my code and cleaned it up to make it more readable

--- food.py
+++ food.py
@@ -1,6 +1,8 @@
 import pygame, random
+
 DEBUG = True
 
+# --------------------------------------- Used for stage 1 --------------------------------------- #
 class Fruit:
     def __init__(self, screen, colour):
         self.screen = screen
@@ -11,15 +13,18 @@ class Fruit:
         self.set_x = set()
         self.set_y = set()
 
+# ------------------------------------ Draws fruit on screen ------------------------------------- #
     def draw(self):
         pygame.draw.rect(self.screen, self.colour, (self.x, self.y, self.size, self.size))
         self.set_x = set(range(self.x, self.x + self.size + 1))
         self.set_y = set(range(self.y, self.y + self.size))
 
+# ------------------------------------ Updates fruit x and y ------------------------------------- #
     def update_xy(self):
         self.x = random.randrange(self.screen.get_width() - self.size)
         self.y = random.randrange(self.screen.get_height() - self.size)
 
+# ---------------------------------- Checks if fruit is eaten ------------------------------------ #
     def is_eaten(self, snake_x, snake_y, snake_size):
         if set(range(snake_x, snake_x + snake_size + 1)) & self.set_x and set(range(snake_y, snake_y + snake_size + 1)) & self.set_y:
             eaten = True
@@ -28,18 +33,21 @@ class Fruit:
         return eaten
 
 
+# --------------------------------------- Used for stage 2 --------------------------------------- #
 class GoldenApple(Fruit):
     def __init__(self, screen, colour):
         Fruit.__init__(self, screen, colour)
         self.size = 60
 
 
+# --------------------------------------- Used for stage 3 --------------------------------------- #
 class PoisonousApple(Fruit):
     def __init__(self, screen, colour):
         Fruit.__init__(self, screen, colour)
         self.size = 45
         self.timer = 0
 
+# ---------------------------------- Gets apple to follow snake ---------------------------------- #
     def follow_snake(self, snakex, snakey):
         self.timer += 1
         
@@ -53,6 +61,8 @@ class PoisonousApple(Fruit):
         elif self.y > snakey:
             self.y -= 5
 
+
+# --------------------------------------- Used for stage 4 --------------------------------------- #
 class VictoryFruit(Fruit):
     def __init__(self, screen, colour):
         Fruit.__init__(self, screen, colour)
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from particle_system import ParticleSystem
 
 DEBUG = False
 
+# ---------------------------- Handles the menus from the menus module --------------------------- #
 def menu_handler(menu_mode, screen, clock, text_font, colour, score, game_won):
     settings_options = {
         "game_difficulty": "easy",
@@ -14,7 +15,8 @@ def menu_handler(menu_mode, screen, clock, text_font, colour, score, game_won):
     in_menu = True
 
     while in_menu:
-        
+
+# ------------------------------------------ Start menu ------------------------------------------ #
         if menu_mode == str("start"):
             start_menu = menus.StartMenu(screen, clock, text_font, colour)
             start_menu.setup()
@@ -39,8 +41,9 @@ def menu_handler(menu_mode, screen, clock, text_font, colour, score, game_won):
                     if DEBUG: print("quit game")
                     pygame.quit()
                     quit()
-                    
-        if menu_mode == str("game over"):            
+
+# ---------------------------------------- Game over menu ---------------------------------------- #                    
+        elif menu_mode == str("game over"):            
             game_over_menu = menus.GameOverMenu(screen, clock, text_font, colour, game_won)
             game_over_menu.setup()
             while game_over_menu.option == None:
@@ -57,7 +60,8 @@ def menu_handler(menu_mode, screen, clock, text_font, colour, score, game_won):
 
                 elif game_over_menu.option == "high score":
                     print(score)
-                    
+
+# ---------------------------------------- Settings menu ----------------------------------------- #
         elif menu_mode == str("settings"):
             settings_menu = menus.SettingsMenu(screen, clock, text_font, colour)
             settings_menu.setup()
@@ -81,19 +85,7 @@ def menu_handler(menu_mode, screen, clock, text_font, colour, score, game_won):
                     menu_mode = str("start")
                     break
 
-        elif menu_mode == str("credits"):
-            credits_menu = menus.CreditsMenu(screen, clock, text_font, colour)
-            credits_menu.setup()
-
-            while credits_menu.option == None:
-                credits_menu.update()
-                credits_menu.is_clicked()
-
-                if credits_menu.option == "back":
-                    if DEBUG: print("start menu")
-                    menu_mode = str("start")
-                    break
-
+# ---------------------------------- Options from settings menu ---------------------------------- #
         elif menu_mode == str("snake colour"):
             snake_colour_menu = menus.SnakeColourMenu(screen, clock, text_font, colour)
             snake_colour_menu.setup()
@@ -173,10 +165,26 @@ def menu_handler(menu_mode, screen, clock, text_font, colour, score, game_won):
                     menu_mode = str("start")
                     if DEBUG: print("menu mode set to start")
                     break
+# ----------------------------------------- Credits menu ----------------------------------------- #
+        elif menu_mode == str("credits"):
+            credits_menu = menus.CreditsMenu(screen, clock, text_font, colour)
+            credits_menu.setup()
+
+            while credits_menu.option == None:
+                credits_menu.update()
+                credits_menu.is_clicked()
+
+                if credits_menu.option == "back":
+                    if DEBUG: print("start menu")
+                    menu_mode = str("start")
+                    break
 
     return settings_options
 
+# --------------------------------------------- Main --------------------------------------------- #
 def main():
+
+# ------------------------------- Initialising variable/constants -------------------------------- #
     WIDTH = 800
     HEIGHT = 800
     SIZE = (WIDTH, HEIGHT)
@@ -191,14 +199,16 @@ def main():
     ORANGE = (255,69,0)
     fps = 25
     score = 0
-    
+
+# ------------------------------ Initialising pygame related items ------------------------------- #
     pygame.init()
     pygame.mixer.init()
     
     screen = pygame.display.set_mode(SIZE)
     clock = pygame.time.Clock()
     text_font = pygame.font.SysFont("Arial", 20)
-    
+
+# ---------------------------- Starting menu and getting user options ---------------------------- #
     menu_mode = str("start")
     settings_options = menu_handler(menu_mode, screen, clock, text_font, WHITE, score, False)
 
@@ -212,7 +222,8 @@ def main():
         "orange": ORANGE,
         "blueberry": BLUE
     }
-    
+
+# ---------------------------- Creating instances of all the classes ----------------------------- #
     snake = Snake(screen, snake_colour, game_difficulty)
     fruit = food.Fruit(screen, fruit_colour[fruit_type])
     golden_apple = food.GoldenApple(screen, GOLD)
@@ -220,6 +231,7 @@ def main():
     victory_fruit = food.VictoryFruit(screen, WHITE)
     particle_system = ParticleSystem(screen)
 
+# ----------------------------------- Setting up stage system ------------------------------------ #
     trigger = False
     stage_1 = True
     stage_2 = False
@@ -227,6 +239,8 @@ def main():
     stage_4 = False
     stage_5 = False
     particle_colour = None
+
+# ----------------------------------- Setting up music system ------------------------------------ #
 
     burp_1 = pygame.mixer.Sound("sounds/burp_1.wav")
     burp_2 = pygame.mixer.Sound("sounds/burp_2.wav")
@@ -236,7 +250,10 @@ def main():
     music = pygame.mixer.music.load("sounds/Cyberpunk_Moonlight_Sonata.mp3")
     pygame.mixer.music.play(-1)
 
-    while not snake.is_dead or not snake.game_won:
+# ---------------------------------------- Main game loop ---------------------------------------- #
+    while not snake.is_dead and not snake.game_won:
+
+# ---------------------------------------- Event handling ---------------------------------------- #
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -256,6 +273,11 @@ def main():
             if snake.direction != 'u':
                 snake.direction = 'd'
 
+        if snake.x < 0 or snake.x > WIDTH or snake.y < 0 or snake.y > HEIGHT:
+            print("collision detected")
+            snake.is_dead = True
+
+# ---------------------------------------- Stage checker ----------------------------------------- #
         if score > 10:
             stage_2 = True
             stage_1 = False
@@ -271,12 +293,10 @@ def main():
         if snake.game_won:
             stage_5 = True
             stage_4 = False
-          
-        if snake.x < 0 or snake.x > WIDTH or snake.y < 0 or snake.y > HEIGHT:
-            snake.is_dead = True
-        
+
         screen.fill(BLACK)
 
+# ---------------------------------------- Stage manager ----------------------------------------- #
         if stage_1:
             if DEBUG: print("stage 1")
             fruit.draw()
@@ -340,6 +360,7 @@ def main():
             snake.game_won = True
             snake.is_dead = True
 
+# ------------------------------------- Outputting to screen ------------------------------------- #
         snake.move(snake.direction)
         particle_system.explode(trigger, snake.x, snake.y, particle_colour)
         screen.blit(text_font.render(f'score: {score}', True, WHITE), (10, 10))
@@ -349,6 +370,7 @@ def main():
         pygame.display.update()
         clock.tick(fps)
 
+# ------------------------------------ End of main game loop ------------------------------------- #
     pygame.mixer.fadeout(1)
     menu_mode = str("game over")
     menu_handler(menu_mode, screen, clock, text_font, WHITE, score, snake.game_won)
